aa2f0d0115d08b7673601ddce0571a250dbd4c72
Roman Rudyak <roman.rudyak@eleks.com>
2018-12-13 18:16:42 +0200

    Implemented firs requirment

--- TDD/StringCalculator.cs
+++ TDD/StringCalculator.cs
@@ -5,6 +5,19 @@ namespace TDD
     {
         public Int32 Add(String numbers)
         {
+            String[] numbersArray = numbers.Split(',', StringSplitOptions.RemoveEmptyEntries);
+            if (numbersArray.Length > 2)
+            {
+                throw new InvalidOperationException("Up to 2 numbers separated by comma (,) are allowed");
+            }
+            else
+            {
+                foreach (String number in numbersArray)
+                {
+                    // If it is not a number, parseInt will throw an exception
+                    Int32.Parse(number);
+                }
+            }
             return 0;
         }
     }
