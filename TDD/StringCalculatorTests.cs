d49df63555649a23b0d4e1b225caf7cda4bb9d9f
Roman Rudyak <roman.rudyak@eleks.com>
2018-12-13 19:41:43 +0200

    Add Unit test for First requirement
     Add_WhenMoreThan2NumbersAreUsedThenExceptionIsThrown
    - Add_When2NumbersAreUsedThenNoExceptionIsThrown
    - Add_WhenNonNumberIsUsedThenExceptionIsThrown
    
    Solution not compiles because not Add method

--- TDD/StringCalculatorTests.cs
+++ TDD/StringCalculatorTests.cs
@@ -1,10 +1,49 @@
-﻿using System;
+using System;
+using Xunit;
+#pragma warning disable CS1701 // Assuming assembly reference matches identity
+
 namespace TDD
 {
     public class StringCalculatorTests
     {
-        public StringCalculatorTests()
+
+        #region Requirement 1: The method can take 0, 1 or 2 numbers separated by comma (,)
+        [Fact]
+        public void Add_WhenMoreThan2NumbersAreUsedThenExceptionIsThrown()
         {
+            // Arrange
+            var calc = new StringCalculator();
+
+            // Act
+            Action act = () => calc.Add("1,2,3");
+
+            // Assert
+            Assert.Throws<InvalidOperationException>(act);
         }
+
+        [Fact]
+        public void Add_When2NumbersAreUsedThenNoExceptionIsThrown()
+        {
+            // Arrange
+            var calc = new StringCalculator();
+
+            // Act
+            calc.Add("1,2");
+        }
+
+        [Fact]
+        public void Add_WhenNonNumberIsUsedThenExceptionIsThrown()
+        {
+            // Arrange
+            var calc = new StringCalculator();
+
+            // Act
+            Action act = () => calc.Add("1,x");
+
+            // Assert
+            Assert.Throws<FormatException>(act);
+        }
+        #endregion Requirement 1: The method can take 0, 1 or 2 numbers separated by comma (,)
     }
 }
+#pragma warning restore CS1701 // Assuming assembly reference matches identity
