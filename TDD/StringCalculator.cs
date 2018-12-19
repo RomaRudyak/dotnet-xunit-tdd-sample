3ac4db5421d36f39c90ad5850426c9ce83e6bb1d
Roman Rudyak <roman.rudyak@eleks.com>
2018-12-19 14:43:00 +0200

    Implemented support for different delimiters

--- TDD/StringCalculator.cs
+++ TDD/StringCalculator.cs
@@ -5,8 +5,21 @@ namespace TDD
     {
         public Int32 Add(String numbers)
         {
+            String[] delimiters = new[] { ",", "\n" };
+            String numbersWithoutDelimiter = numbers;
+            if (numbers.StartsWith("//", StringComparison.InvariantCulture))
+            {
+                int delimiterIndex = numbers.IndexOf("//", StringComparison.InvariantCulture) + 2;
+                delimiters = new[] { numbers.Substring(delimiterIndex, 1) };
+                numbersWithoutDelimiter = numbers.Substring(numbers.IndexOf("\n", StringComparison.InvariantCulture) + 1);
+            }
+            return Add(numbersWithoutDelimiter, delimiters);
+        }
+
+        private static int Add(string numbers, String[] delimeters)
+        {
             var result = 0;
-            String[] numbersArray = numbers.Split(new[] { ",", "\n" }, StringSplitOptions.RemoveEmptyEntries);
+            String[] numbersArray = numbers.Split(delimeters, StringSplitOptions.RemoveEmptyEntries);
             foreach (String number in numbersArray)
             {
                 if (String.IsNullOrEmpty(number))
