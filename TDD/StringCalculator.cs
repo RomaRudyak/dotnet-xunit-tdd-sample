37bc60ac6f6aa8f92c22000cceca415b854626c6
Roman Rudyak <roman.rudyak@eleks.com>
2018-12-19 13:05:02 +0200

    Add implementation for meat requirements

--- TDD/StringCalculator.cs
+++ TDD/StringCalculator.cs
@@ -5,6 +5,7 @@ namespace TDD
     {
         public Int32 Add(String numbers)
         {
+            var result = 0;
             String[] numbersArray = numbers.Split(',', StringSplitOptions.RemoveEmptyEntries);
             if (numbersArray.Length > 2)
             {
@@ -14,11 +15,17 @@ namespace TDD
             {
                 foreach (String number in numbersArray)
                 {
+                    if (String.IsNullOrEmpty(number))
+                    {
+                        result += 0;
+                        continue;
+                    }
                     // If it is not a number, parseInt will throw an exception
                     Int32.Parse(number);
                 }
             }
-            return 0;
+
+            return result;
         }
     }
 }
